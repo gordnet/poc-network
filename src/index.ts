dc129ff2586304ccf8cb28e6601a6a1dce61229d
Shamoon Siddiqui <shamoon@develop.io>
2021-12-02 09:49:27 -0500

    Trying second level node

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import http from 'http'
 import https from 'https'
-import levelup from 'levelup'
-import leveldown from 'leveldown'
 import { program } from 'commander'
-// import { getPeers } from './utils/peers'
 
 program
   .description('A node in the network')
@@ -12,11 +9,10 @@ program
 program.parse();
 
 const options = program.opts()
-// const CWD = process.cwd()
 const PORT = options.port as number || 10309
-// const peerDb = levelup(leveldown(`${CWD}/data/peers.${global.PORT}`))
 
 const requestHandler = (request: http.IncomingMessage, response: http.ServerResponse) => {
+  console.log('I GOT SOMETHING!!!!')
   if (request.headers['content-type'] !== 'application/json') {
     return response.writeHead(400).end('Invalid content-type')
   }
@@ -25,46 +21,71 @@ const requestHandler = (request: http.IncomingMessage, response: http.ServerResp
   request.on('data', chunk => {
     data += chunk;
   })
+
   request.on('end', () => {
-
+    let requestData: any = {}
     try {
-      const requestData = JSON.parse(data)
-      const [protocol, urlPath] = requestData.destination.split('://')
-
-      const requestOptions = {
-        hostname: urlPath.split('/')[0],
-        port: protocol === 'https' ? 443 : 80,
-        path: urlPath.split('/').slice(1).join('/'),
-        method: 'GET'
-      }
-
-      const httpLibrary = protocol === 'https' ? https : http
-      console.log({ requestOptions })
-      const newRequest = httpLibrary.request(requestOptions, async (res) => {
-        console.log(`statusCode: ${res.statusCode}`)
-
-        res.on('data', d => {
-          console.log('data', d.toString())
-          response.write(d)
-        })
-
-        res.on('end', () => {
-          response.end()
-        })
-      })
-      console.log('after')
-
-      newRequest.on('error', error => {
-        console.error(error)
-      })
-
-      newRequest.end()
-
-      console.log({ requestData })
+      requestData = JSON.parse(data)
     } catch (e) {
-      console.log(e)
       return response.writeHead(400).end('Invalid JSON')
     }
+
+    const [protocol, urlPath] = requestData.destination.split('://')
+
+    let requestOptions: any = {
+      hostname: urlPath.split('/')[0],
+      port: protocol === 'https' ? 443 : 80,
+      path: urlPath.split('/').slice(1).join('/'),
+      method: 'GET'
+    }
+
+    let destinationData = ''
+
+
+    if (requestData.nodes?.length > 0) {
+      // We need to forward this request to another node
+      const destination = requestData.nodes[0]
+
+      destinationData = JSON.stringify({
+        destination: requestData.destination,
+        nodes: requestData.nodes.slice(1),
+      })
+
+      requestOptions.hostname = destination.split(':')[0]
+      requestOptions.port = destination.split(':')[1]
+      delete requestOptions.path
+      requestOptions.method = 'POST'
+      requestOptions.headers = {
+        'Content-Type': 'application/json',
+        'Content-Length': destinationData.length
+      }
+    }
+
+    const httpLibrary = protocol === 'https' ? https : http
+
+    console.log({ requestOptions })
+
+    const newRequest = httpLibrary.request(requestOptions, async (res) => {
+      res.on('data', (d) => {
+        response.write(d)
+      })
+
+      res.on('end', () => {
+        response.end()
+      })
+    })
+
+    newRequest.on('error', error => {
+      console.log({ error })
+      return response.writeHead(500).end(error.message)
+    })
+
+    if (destinationData?.length > 0) {
+      console.log({ destinationData })
+      newRequest.write(destinationData)
+    }
+
+    newRequest.end()
   })
 
 }
